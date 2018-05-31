d5266cd0487bc70c81dba640a4ae6c21cd2ccec0
Yuusaku Hayashi <forestmtree@gmail.com>
2018-05-31 23:39:05 +0900

    lesson1-2 fixed

--- src/lesson1-2.c
+++ src/lesson1-2.c
@@ -14,7 +14,7 @@ int main(void){
 	fahr = lower;
 	while(fahr <= upper){
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%dn", fahr, celsius);
+		printf("%d\t%d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 	return 0;
