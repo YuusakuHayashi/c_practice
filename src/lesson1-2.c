0a5e471c5cfd382348dee9a7e475910d187798cf
Yuusaku Hayashi <forestmtree@gmail.com>
2018-06-02 00:07:18 +0900

    fixed in detail

--- src/lesson1-2.c
+++ src/lesson1-2.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main(void){
-	int fahr;
-	int celsius;
-	int lower;
-	int upper;
-	int step;
+	float fahr;
+	float celsius;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-	while(fahr <= upper){
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
-		fahr = fahr + step;
+	fahr = LOWER;
+	while(fahr <= UPPER){
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
+		fahr = fahr + STEP;
 	}
 	return 0;
 }
